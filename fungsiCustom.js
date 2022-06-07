2bcb43ec8ba75a04cb7ee4b492e171c555ea075a
Dimyati Utoyo <dimyatiutoyo.b@gmail.com>
2022-06-07 23:59:40 +0700

    some fix and add promise

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,4 @@
-// TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -16,9 +16,56 @@ let modifyFile3 = (val) => {
   file3 = val;
 };
 
-// TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let arrayOfString = [];
+  try {
+
+    // create a promise
+    const promise = new Promise((resolve, reject) => {
+      fs.readFile(file1, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const json1 = JSON.parse(data);
+          arrayOfString.push(json1.message.split(' ')[1]);
+        }
+      });
+
+      fs.readFile(file2, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const json2 = JSON.parse(data);
+          arrayOfString.push(json2[0].message.split(' ')[1]);
+        }
+      });
+
+      fs.readFile(file3, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const json3 = JSON.parse(data);
+          arrayOfString.push(json3[0].data.message.split(' ')[1]);
+        }
+
+        // resolve the promise
+        resolve(arrayOfString);
+      });
+    });
+
+
+    // if the promise is resolved, then call the callback function
+    promise.then((data) => {
+      fnCallback(null, data);
+    }).catch((err) => {
+      fnCallback(err, null);
+    });
+  } catch (err) {
+    fnCallback(err, null);
+  }
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
